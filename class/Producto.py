61b506ba2c40d2d4cab54e73ac67092a98dcf377
sdparada97 <sdparada97@gmail.com>
2021-03-08 14:36:00 -0500

    Inicio proyecto Market : Creacion clase Producto

---
+++ class/Producto.py
@@ -0,0 +1,10 @@
+
+# CLASE -----------> PRODUCTO
+class Producto:
+
+    #Constructor -----> PRODUCTO
+    def __init__(self,nombre, precio_valor, cantidad, descuento):
+        self.nombre = nombre
+        self.precio_valor = precio_valor
+        self.cantidad = cantidad
+        self.descuento = descuento
