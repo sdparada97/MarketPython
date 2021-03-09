7f64913920909ba8a5ba20ff33ed12ebd61d55b5
sdparada97 <sdparada97@gmail.com>
2021-03-09 17:58:13 -0500

    Creacion clase producto

--- Clases/Productos.py
+++ Clases/Productos.py
@@ -2,11 +2,41 @@
 # CLASE -----------> PRODUCTO
 class Producto:
 
+    descuento = 0
+    precioDescuento = 0
+
     #Constructor -----> PRODUCTO
-    def __init__(self,nombre, precio_valor, cantidad, descuento):
+    def __init__(self,nombre, precio_valor, cantidad):
         self.nombre = nombre
         self.precio_valor = precio_valor
         self.cantidad = cantidad
-        self.descuento = descuento
 
     #Metodos -----> PRODUCTO
+    def PrecioConDescuento(self,descuento):
+        valorDescuento = self.precio_valor * self.descuento
+        self.precioDescuento = self.precio_valor - valorDescuento
+    
+    def AgregarDescuento(self,descuento):
+        if( descuento>=0 and descuento<=50):
+            self.descuento = descuento/100
+            self.PrecioConDescuento(descuento)
+        else:
+            print("ESE PORCENTAJE NO SE ENCUENTRA DENTRO DEL RANGO (0 % - 50 %)")
+
+    def __str__(self):
+        str_producto = ""
+        if(self.descuento != 0):
+            str_producto = """
+        NOMBRE:                 \t{}
+        PRECIO:                 \t{}
+        CANTIDAD:               \t{}
+        DESCUENTO:              \t{}%
+        PRECIO CON DESCUENTO:   \t{}""".format(self.nombre,self.precio_valor,self.cantidad,self.descuento*100,self.precioDescuento)
+        else:
+            str_producto = """
+        NOMBRE:               \t{}
+        PRECIO:               \t{}
+        CANTIDAD:             \t{}""".format(self.nombre,self.precio_valor,self.cantidad)
+        
+        return str_producto
+
--- main_market.py
+++ main_market.py
@@ -1,6 +1,9 @@
 import Clases.Empleados
+import Clases.Productos
 
 cajero1 = Clases.Empleados.Cajero("SERGIO","1016095700","3057752461","INDEFINIDO",1500000,0)
-#Cajero
-
 print(cajero1)
+
+producto1 = Clases.Productos.Producto("LIBRO: EL PRINCIPITO", 45000, 20)
+producto1.AgregarDescuento(10)
+print(producto1)
